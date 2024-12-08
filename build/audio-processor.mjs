3495bf67c38b7f9993a7db8727eddc69dfe938d8
gfljs2101 <gfljs2100.user@gmail.com>
2024-12-08 17:48:18 +0800

    Update audio-processor.mjs

--- build/audio-processor.mjs
+++ build/audio-processor.mjs
@@ -94,7 +94,7 @@ class e extends AudioWorkletProcessor {
                     return this.lastByteValue[t] = Math.round(127.5 * (s + 1)), s
                 };
                 break;
-            case "Signed Bytebeat":
+            case "Logmode":
                 this.getValues = (e, t) => (this.lastByteValue[t] = log2(e) * 32 & 255) / 127.5 - 1;
                 break;
             default:
